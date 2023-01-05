4749c12b9e658cb6ede2ee3258105d08b30afa4d
George Beckert <georgebeckert@gmail.com>
2023-01-05 14:54:10 -0500

    make flower canvas background

--- assets/javascript/flowercanvas.js
+++ assets/javascript/flowercanvas.js
@@ -1,36 +1,40 @@
 var flowers = [];
-var addFlowerCount = 50;
 var flowerCanvas = null;
 var myContext = null;
+var maxFlowers = 15;
+var scrollCount = 4;
+var currentScrollCount = 0;
 function startCanvas() {
-  flowerCanvas = document.getElementById('flowerCanvas');
-  flowerCanvasContainer = document.getElementById('flowerCanvasContainer');
-  myContext = flowerCanvas.getContext('2d');
-  myContext.fillRect(0, 0, flowerCanvas.width, flowerCanvas.height);
-  var theHeader = document.getElementById('header');
+  flowerCanvas = document.getElementById("flowerCanvas");
+  flowerCanvasContainer = document.getElementById("flowerCanvasContainer");
+  myContext = flowerCanvas.getContext("2d");
   const tempWidth = flowerCanvasContainer.clientWidth;
+  const tempHeight = flowerCanvasContainer.clientHeight;
   myContext.canvas.width = tempWidth;
-  myContext.canvas.height = tempWidth + 4;
-  addRandomFlowers(5, myContext);
-  setInterval(function() {
-    updateCanvas();
-  }, 100);
-  window.onresize = function(event) {
+  myContext.canvas.height = tempHeight;
+  window.onresize = function (_e) {
     myContext.canvas.width = tempWidth;
-    myContext.canvas.height = tempWidth;
+    myContext.canvas.height = tempHeight;
   };
+  window.onscroll = function (_e) {
+    currentScrollCount++;
+    if (currentScrollCount === scrollCount) {
+      currentScrollCount = 0;
+      updateCanvas();
+    }
+  };
+  updateCanvas();
 }
 
-function addFlower(x, y, width, height, curveWeight, number, color, growRate) {
-  flowers.push([x, y, width, height, curveWeight, number, color, growRate]);
+function addFlower(flower) {
+  flowers.push(flower);
 }
-function drawFlower(x, y, width, height, curveWeight, number, color) {
+function drawFlower({ x, y, width, height, curveWeight, petalCount, color }) {
   var TO_RADIANS = Math.PI / 180;
   myContext.translate(x, y);
-  myContext.fillStyle = '#42f495';
-  myContext.fillRect(-2.5, 0, 5, flowerCanvas.height - y);
-  var rotation = 0;
-  for (var j = 0; j < number; j++) {
+  myContext.fillStyle = "#73ba8d";
+  myContext.fillRect(-10, 0, 20, flowerCanvas.height - y);
+  for (var j = 0; j < petalCount; j++) {
     myContext.fillStyle = color;
     myContext.beginPath();
     myContext.moveTo(0, 0);
@@ -38,65 +42,64 @@ function drawFlower(x, y, width, height, curveWeight, number, color) {
     myContext.moveTo(0, 0);
     myContext.quadraticCurveTo(curveWeight * width, -1 * height, width, 0);
     myContext.fill();
-    myContext.rotate(TO_RADIANS * (360 / number));
+    myContext.rotate(TO_RADIANS * (360 / petalCount));
   }
-  myContext.rotate(TO_RADIANS * (360 - (360 / number) * j));
-  myContext.fillStyle = '#fced7e';
+  myContext.rotate(TO_RADIANS * (360 - (360 / petalCount) * j));
+  myContext.fillStyle = "#fced7e";
   myContext.beginPath();
-  myContext.arc(0, 0, 10, 0, 2 * Math.PI);
+  myContext.arc(0, 0, 15, 0, 2 * Math.PI);
   myContext.fill();
   myContext.translate(-x, -y);
 }
-function addRandomFlowers(numberOfFlowers) {
+function createRandomFlowers(numberOfFlowers) {
+  const flowers = [];
   for (var i = 0; i < numberOfFlowers; i++) {
-    var randomColor = '#000000'.replace(/0/g, function() {
+    const randomColor = "#000000".replace(/0/g, function () {
       return (~~(Math.random() * 16)).toString(16);
     });
-    addFlower(
-      Math.random() * flowerCanvas.width, //x
-      flowerCanvas.height - flowerCanvas.height * 0.1, //y
-      Math.random() * 30 + (500 / flowerCanvas.width) * 10, //width
-      Math.random() * 30 + (500 / flowerCanvas.height) * 10, //height
-      Math.random() * 2, //curve weight
-      Math.floor(Math.random() * 30) / 2 + 3, //number
-      randomColor + 'AA', //color
-      Math.random() * 0.5 + 0.5, //grow rate
-    );
+    const initialY = flowerCanvas.height -200;
+    flowers.push({
+      x: Math.random() * flowerCanvas.width, //x
+      y: initialY, //y
+      width: Math.random() * 200 + 100, //width
+      height: Math.random() * 200 + 100, //height
+      curveWeight: Math.random() * 2, //curve weight
+      petalCount: Math.floor(Math.random() * 30) / 2 + 3, //petalCount
+      color: `${randomColor}AA`, //color
+      growthRate: Math.random() * 60 + 30, //grow rate
+      growthProb: 0.5,
+      maxHeight: 200,
+    });
   }
+  return flowers;
 }
 function updateCanvas() {
-  var primaryColor = '#87ceeb';
-  var secondaryColor = '#3F3931';
+  active = false;
+  var primaryColor = "#fdfdf1";
+  var secondaryColor = "#3F3931";
   myContext.fillStyle = primaryColor;
   myContext.fillRect(0, 0, flowerCanvas.width, flowerCanvas.height);
+
   //add more flowers as time passes;
-  if (addFlowerCount <= 0) {
-    addFlowerCount = 100 + Math.floor(Math.random() * 100);
-    addRandomFlowers(1);
-  } else {
-    addFlowerCount--;
-  }
-  //update flowers
-  for (var k = 0; k < flowers.length; k++) {
-    if (flowers[k][1] >= flowerCanvas.height / 2) {
-      flowers[k][1] = flowers[k][1] - flowers[k][7];
-      drawFlower(
-        flowers[k][0],
-        flowers[k][1],
-        flowers[k][2],
-        flowers[k][3],
-        flowers[k][4],
-        flowers[k][5],
-        flowers[k][6],
-      );
-    } else {
-      flowers.splice(k, 1);
-      addRandomFlowers(1);
+  if (flowers.length < maxFlowers) {
+    if (Math.random() < 0.3) {
+      flowers.push(createRandomFlowers(1)[0]);
     }
   }
+  //update flowers
+  flowers.forEach((flower, i) => {
+    if (Math.random() < flower.growthProb) {
+      flower.y -= flower.growthRate;
+    }
+    drawFlower(flower);
+  });
+
+  flowers = flowers.map((flower) =>
+    flower.y >= flower.maxHeight ? flower : createRandomFlowers(1)[0]
+  );
   myContext.fillStyle = secondaryColor;
   myContext.fillRect(0, flowerCanvas.height - 10, flowerCanvas.width, 10);
 }
-window.onload = function() {
+window.onload = function () {
   startCanvas();
 };
